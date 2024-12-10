ecc176924fe5ca74afb2bbda3f7f5df76ec90bb7
Christoffer Wallén <36072821+chwallen@users.noreply.github.com>
2024-12-10 20:53:00 +0100

    add solutions for 2024, day 10

---
+++ 2024/day10/day10_test.go
@@ -0,0 +1,65 @@
+package day10_test
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	day "github.com/chwallen/advent-of-code/2024/day10"
+	"github.com/chwallen/advent-of-code/internal/test"
+)
+
+//go:embed input.txt
+var input string
+
+func TestDay(t *testing.T) {
+	exampleInput := strings.Split(
+		`89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`,
+		"\n",
+	)
+
+	realInput := strings.Split(input[0:len(input)-1], "\n")
+
+	tests := []test.Test{
+		{
+			Name:     "part 1 example",
+			DayPart:  day.PartOne,
+			Input:    exampleInput,
+			Expected: 36,
+		},
+		{
+			Name:     "part 1 real",
+			DayPart:  day.PartOne,
+			Input:    realInput,
+			Expected: 789,
+		},
+		{
+			Name:     "part 2 example",
+			DayPart:  day.PartTwo,
+			Input:    exampleInput,
+			Expected: 81,
+		},
+		{
+			Name:     "part 2 real",
+			DayPart:  day.PartTwo,
+			Input:    realInput,
+			Expected: 1735,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			actual := test.DayPart(test.Input, test.Extras...)
+			if actual != test.Expected {
+				t.Errorf("Expected %d, actual %d", test.Expected, actual)
+			}
+		})
+	}
+}
