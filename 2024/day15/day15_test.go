98baffb5fe673982b5602e286b5faca7e7f23d3c
Christoffer Wallén <36072821+chwallen@users.noreply.github.com>
2024-12-15 20:59:58 +0100

    add solutions for 2024, day 15

---
+++ 2024/day15/day15_test.go
@@ -0,0 +1,78 @@
+package day15_test
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	day "github.com/chwallen/advent-of-code/2024/day15"
+	"github.com/chwallen/advent-of-code/internal/test"
+)
+
+//go:embed input.txt
+var input string
+
+func TestDay(t *testing.T) {
+	exampleInput := strings.Split(
+		`##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`,
+		"\n",
+	)
+
+	realInput := strings.Split(input[0:len(input)-1], "\n")
+
+	tests := []test.Test{
+		{
+			Name:     "part 1 example",
+			DayPart:  day.PartOne,
+			Input:    exampleInput,
+			Expected: 10092,
+		},
+		{
+			Name:     "part 1 real",
+			DayPart:  day.PartOne,
+			Input:    realInput,
+			Expected: 1492518,
+		},
+		{
+			Name:     "part 2 example",
+			DayPart:  day.PartTwo,
+			Input:    exampleInput,
+			Expected: 9021,
+		},
+		{
+			Name:     "part 2 real",
+			DayPart:  day.PartTwo,
+			Input:    realInput,
+			Expected: 1512860,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			actual := test.DayPart(test.Input, test.Extras...)
+			if actual != test.Expected {
+				t.Errorf("Expected %d, actual %d", test.Expected, actual)
+			}
+		})
+	}
+}
