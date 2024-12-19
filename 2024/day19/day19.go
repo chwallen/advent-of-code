1a1759d1554a7fd2172a16ccc21dcfd5d4f25fa8
Christoffer Wallén <36072821+chwallen@users.noreply.github.com>
2024-12-19 20:49:03 +0100

    add solutions for 2024, day 19

---
+++ 2024/day19/day19.go
@@ -0,0 +1,146 @@
+package day19
+
+import (
+	"strings"
+
+	"github.com/chwallen/advent-of-code/internal/ds"
+)
+
+type node struct {
+	white   *node
+	blue    *node
+	black   *node
+	red     *node
+	green   *node
+	pattern string
+}
+
+func PartOne(lines []string, extras ...any) any {
+	rootNode := createTree(lines[0])
+
+	knownDesigns := ds.NewSet[string]()
+	for _, pattern := range strings.Split(lines[0], ", ") {
+		knownDesigns.Add(pattern)
+	}
+
+	possibleDesigns := 0
+	for _, line := range lines[2:] {
+		if isDesignPossible(rootNode, line, knownDesigns) {
+			possibleDesigns += 1
+		}
+	}
+	return possibleDesigns
+}
+
+func isDesignPossible(root *node, design string, knownDesigns ds.Set[string]) bool {
+	if knownDesigns.Contains(design) {
+		return true
+	}
+
+	currentNode := root
+	for _, char := range design {
+		currentNode = currentNode.getChild(char)
+		if currentNode == nil {
+			return false
+		}
+
+		patternLength := len(currentNode.getPattern())
+		if patternLength > 0 && isDesignPossible(root, design[patternLength:], knownDesigns) {
+			knownDesigns.Add(design)
+			return true
+		}
+	}
+	return false
+}
+
+func PartTwo(lines []string, extras ...any) any {
+	rootNode := createTree(lines[0])
+
+	knownDesigns := make(map[string]int, 20_000)
+	for _, pattern := range strings.Split(lines[0], ", ") {
+		countPossibleDesigns(rootNode, pattern, knownDesigns)
+	}
+
+	possibleDesignCombinations := 0
+	for _, line := range lines[2:] {
+		possibleDesignCombinations += countPossibleDesigns(rootNode, line, knownDesigns)
+	}
+	return possibleDesignCombinations
+}
+
+func countPossibleDesigns(root *node, design string, knownDesigns map[string]int) int {
+	count, isKnownDesign := knownDesigns[design]
+	if isKnownDesign {
+		return count
+	}
+	currentNode := root
+	for _, char := range design {
+		currentNode = currentNode.getChild(char)
+		if currentNode == nil {
+			knownDesigns[design] = count
+			return count
+		} else if design == currentNode.getPattern() {
+			count += 1
+			knownDesigns[design] = count
+			return count
+		}
+		patternLength := len(currentNode.getPattern())
+		if patternLength > 0 {
+			possibleDesigns := countPossibleDesigns(root, design[patternLength:], knownDesigns)
+			knownDesigns[design] = possibleDesigns
+			count += possibleDesigns
+		}
+	}
+	knownDesigns[design] = count
+	return count
+}
+
+func createTree(input string) *node {
+	root := &node{}
+	patterns := strings.Split(input, ", ")
+	for _, pattern := range patterns {
+		currentNode := root
+		for i, char := range pattern {
+			currentNode = currentNode.getOrCreateChild(pattern, char, i)
+		}
+	}
+	return root
+}
+
+func (n *node) getChild(char rune) *node {
+	return *n.getChildField(char)
+}
+
+func (n *node) getPattern() string {
+	return n.pattern
+}
+
+func (n *node) getChildField(char rune) **node {
+	switch char {
+	case 'w':
+		return &n.white
+	case 'u':
+		return &n.blue
+	case 'b':
+		return &n.black
+	case 'r':
+		return &n.red
+	case 'g':
+		return &n.green
+	default:
+		panic("invalid char " + string(char))
+	}
+}
+
+func (n *node) getOrCreateChild(pattern string, char rune, index int) *node {
+	childField := n.getChildField(char)
+	child := *childField
+	if child == nil {
+		child = &node{}
+		*childField = child
+	}
+	if len(pattern)-1 == index {
+		child.pattern = pattern
+	}
+	return child
+}
---
+++ 2024/day19/day19_test.go
@@ -0,0 +1,67 @@
+package day19_test
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	day "github.com/chwallen/advent-of-code/2024/day19"
+	"github.com/chwallen/advent-of-code/internal/test"
+)
+
+//go:embed input.txt
+var input string
+
+func TestDay(t *testing.T) {
+	exampleInput := strings.Split(
+		`r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`,
+		"\n",
+	)
+
+	realInput := strings.Split(input[0:len(input)-1], "\n")
+
+	tests := []test.Test{
+		{
+			Name:     "part 1 example",
+			DayPart:  day.PartOne,
+			Input:    exampleInput,
+			Expected: 6,
+		},
+		{
+			Name:     "part 1 real",
+			DayPart:  day.PartOne,
+			Input:    realInput,
+			Expected: 213,
+		},
+		{
+			Name:     "part 2 example",
+			DayPart:  day.PartTwo,
+			Input:    exampleInput,
+			Expected: 16,
+		},
+		{
+			Name:     "part 2 real",
+			DayPart:  day.PartTwo,
+			Input:    realInput,
+			Expected: 1016700771200474,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			actual := test.DayPart(test.Input, test.Extras...)
+			if actual != test.Expected {
+				t.Errorf("Expected %d, actual %d", test.Expected, actual)
+			}
+		})
+	}
+}
