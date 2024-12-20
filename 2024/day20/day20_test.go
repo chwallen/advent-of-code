0031c8153b8c9306cdea86a481df6c58594295b4
Christoffer Wallén <36072821+chwallen@users.noreply.github.com>
2024-12-20 21:57:33 +0100

    add solutions for 2024, day 20

---
+++ 2024/day20/day20_test.go
@@ -0,0 +1,74 @@
+package day20_test
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	day "github.com/chwallen/advent-of-code/2024/day20"
+	"github.com/chwallen/advent-of-code/internal/test"
+)
+
+//go:embed input.txt
+var input string
+
+func TestDay(t *testing.T) {
+	exampleInput := strings.Split(
+		`###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############`,
+		"\n",
+	)
+
+	realInput := strings.Split(input[0:len(input)-1], "\n")
+
+	tests := []test.Test{
+		{
+			Name:     "part 1 example",
+			DayFunc:  day.PartOne,
+			Input:    exampleInput,
+			Extras:   []any{2},
+			Expected: 44,
+		},
+		{
+			Name:     "part 1 real",
+			DayFunc:  day.PartOne,
+			Input:    realInput,
+			Expected: 1378,
+		},
+		{
+			Name:     "part 2 example",
+			DayFunc:  day.PartTwo,
+			Input:    exampleInput,
+			Extras:   []any{50},
+			Expected: 285,
+		},
+		{
+			Name:     "part 2 real",
+			DayFunc:  day.PartTwo,
+			Input:    realInput,
+			Expected: 975379,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			actual := test.DayFunc(test.Input, test.Extras...)
+			if actual != test.Expected {
+				t.Errorf("Expected %d, actual %d", test.Expected, actual)
+			}
+		})
+	}
+}
